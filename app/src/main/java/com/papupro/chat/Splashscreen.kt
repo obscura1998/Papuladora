54786ad2f9bc31658057f77bcf8db2132fb9b3b3
fate4 <franciscoom82@gmail.com>
2025-06-12 23:27:22 -0600

    dos puntos uve

--- app/src/main/java/com/papupro/chat/Splashscreen.kt
+++ app/src/main/java/com/papupro/chat/Splashscreen.kt
@@ -1,6 +1,8 @@
 package com.papupro.chat
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,22 @@ class Splashscreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splashscreen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        MostrarBienvenida()
     }
+    fun MostrarBienvenida(){
+        object: CountDownTimer(3000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onFinish() {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+
+        }.start()
+    }
+
 }
